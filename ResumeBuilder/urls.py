7c15c837b4f308b64845017a81a6056c91b88a65
Atharv Pathak <47697790+atharvspathak@users.noreply.github.com>
2020-10-09 22:52:41 +0530

    Add files via upload

--- ResumeBuilder/urls.py
+++ ResumeBuilder/urls.py
@@ -1,14 +1,23 @@
+"""ResumeBuilder URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include   #import include
 
 urlpatterns = [
-    path("",views.index,name="index"),                                  #mainpage/loginpage
-    path("createUser",views.createUser),                                #newuserpage
-    path("home",views.homePage,name="Home"),                            #homepage
-    path("cv",views.cvPage,name="CV"),                                  #cvpage
-    path("projects",views.projectsPage,name="Projects"),                #projectpage
-    path("getInformation",views.getInfo,name="User Info"),              #User Inforamtion
-    path("",views.logoutUser,name="logout"),                            #logout          
-  
+    path('admin/', admin.site.urls),
+    path('',include("home.urls")),
+    path('logout',include("home.urls"))
 ]
